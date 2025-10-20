569276725f4b8bbc0d81dfcc9f49ca38559f7f2f
Anita <siddharthjaiswar30495@gmail.com>
2025-10-20 16:07:03 +0530

    fix: revert to stable express version and add postinstall check

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import http from "http"; // ⬅ new
-import { Server } from "socket.io"; // ⬅ new
+import http from "http";
+import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
